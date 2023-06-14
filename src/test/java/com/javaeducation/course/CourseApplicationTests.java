1ca6eac29570ef6d80a94298a531d2fa5576001c
Ulguim <williamulguim2014@gmail.com>
2023-06-14 11:25:44 -0300

    :tada: Create project base

---
+++ src/test/java/com/javaeducation/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaeducation.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
